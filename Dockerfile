12d5e88ad0d8d811088126743091f4ae8e66ed8c
ChungYujoyce <joyce.bhps@gmail.com>
2024-03-12 23:37:54 -0700

    Add vllm server
    Signed-off-by: ChungYujoyce <joyce.bhps@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nvcr.io/nvidia/pytorch:24.01-py3
+FROM nvcr.io/nvidia/pytorch:23.08-py3
 WORKDIR /workspace/
 RUN apt-get update && \
  apt-get install -y poppler-utils
---
+++ vllm_server.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES=0,1 python vllm_server.py \
+    --model=/home/chsieh/joyce/MicroGPT/models/models--mistralai--Mixtral-8x7B-Instruct-v0.1/snapshots/1e637f2d7cb0a9d6fb1922f305cb784995190a83 \
+    --max-model-len=128 \
+    --tensor-parallel-size=2 \
+    --pipeline-parallel-size=1 \
+    --trust-remote-code \
+    --dtype bfloat16 \

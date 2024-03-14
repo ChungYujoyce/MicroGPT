013f899ccabc39158e6014edd4c4990488cf6ed3
Joyce Huang (System) <Joyce_Huang@supermicro.com>
2024-03-13 17:28:24 -0700

    POST,GET,UPDATE,DELETE logic updates, codebase refactoring, add VDB management
    Signed-off-by: Joyce Huang (System) <Joyce_Huang@supermicro.com>

--- localGPTUI/localGPTUI.py
+++ localGPTUI/localGPTUI.py
@@ -16,26 +16,25 @@ API_HOST = "http://localhost:5110/api"
 
 
 # PAGES #
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST", "PUT", "DELETE"])
 def home_page():
     if request.method == "POST":
         if "user_prompt" in request.form:
-            user_prompt = request.form["user_prompt"]
+            user_prompt = request.form.get("user_prompt")
             print(f"User Prompt: {user_prompt}")
 
             main_prompt_url = f"{API_HOST}/prompt_route"
             response = requests.post(main_prompt_url, data={"user_prompt": user_prompt})
-            print(response.status_code)  # print HTTP response status code for debugging
+            print(response)
             if response.status_code == 200:
                 # print(response.json())  # Print the JSON data from the response
                 return render_template("home.html", show_response_modal=True, response_dict=response.json())
         elif "documents" in request.files:
-            delete_source_url = f"{API_HOST}/delete_source"  # URL of the /api/delete_source endpoint
             if request.form.get("action") == "reset":
-                response = requests.get(delete_source_url)
+                delete_source_url = f"{API_HOST}/delete_source"  # URL of the /api/delete_source endpoint
+                response = requests.delete(delete_source_url)
 
             save_document_url = f"{API_HOST}/save_document"
-            run_ingest_url = f"{API_HOST}/run_ingest"  # URL of the /api/run_ingest endpoint
             files = request.files.getlist("documents")
             for file in files:
                 print(file.filename)
@@ -44,10 +43,34 @@ def home_page():
                     f.write(file.read())
                     f.seek(0)
                     response = requests.post(save_document_url, files={"document": (filename, f)})
-                    print(response.status_code)  # print HTTP response status code for debugging
+                    print(response)  # print HTTP response status code for debugging
+                    
+            if request.form.get("action") == "add":
+                run_ingest_url = f"{API_HOST}/run_add"
+
+            if request.form.get("action") == "reset":
+                run_ingest_url = f"{API_HOST}/run_reset"  # URL of the /api/run_ingest endpoint
             # Make a GET request to the /api/run_ingest endpoint
-            response = requests.get(run_ingest_url)
-            print(response.status_code)  # print HTTP response status code for debugging
+            response = requests.post(run_ingest_url)
+            print(response)
+            
+    elif request.method == "PUT":
+        if "editInput" in request.form:
+            id, revise_result = request.form.get("id"), request.form.get("revise_result")
+            print(f"id: {id}")
+            print(f"Revised content: {revise_result}")
+            revise_result_url = f"{API_HOST}/run_update"
+            response = requests.put(revise_result_url, data={"id": id, "revise_result": revise_result})
+            print(response.content)
+        
+    elif request.method == "DELETE":
+        if "deleteInput" in request.form:
+            id = request.form.get("id"),
+            print(f"id: {id}")
+            revise_result_url = f"{API_HOST}/run_delete"
+            response = requests.delete(revise_result_url, data={"id": id})
+            print(response.content)
+            
 
     # Display the form for GET request
     return render_template(
@@ -59,11 +82,11 @@ def home_page():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--port", type=int, default=5111, help="Port to run the UI on. Defaults to 5111.")
+    parser.add_argument("--port", type=int, default=3111, help="Port to run the UI on. Defaults to 5111.")
     parser.add_argument(
         "--host",
         type=str,
-        default="127.0.0.1",
+        default="0.0.0.0",
         help="Host to run the UI on. Defaults to 127.0.0.1. "
         "Set to 0.0.0.0 to make the UI externally "
         "accessible from other devices.",

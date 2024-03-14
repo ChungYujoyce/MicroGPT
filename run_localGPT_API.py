9fb61a883cdb6bbdf1ff07934add20a23e5bb0ff
Joyce Huang (System) <Joyce_Huang@supermicro.com>
2024-03-13 17:46:09 -0700

    same logic as run_localGPT.py, but on web backend side, handling requests
    Signed-off-by: Joyce Huang (System) <Joyce_Huang@supermicro.com>

--- run_localGPT_API.py
+++ run_localGPT_API.py
@@ -15,9 +15,13 @@ from prompt_template_utils import get_prompt_template
 
 # from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
+from langchain.docstore.document import Document
+from langchain_community.retrievers import BM25Retriever
+from utils import clean_text
 from werkzeug.utils import secure_filename
 
-from constants import CHROMA_SETTINGS, EMBEDDING_MODEL_NAME, PERSIST_DIRECTORY, MODEL_ID, MODEL_BASENAME
+from constants import CHROMA_SETTINGS, EMBEDDING_MODEL_NAME, PERSIST_DIRECTORY, PARSED_DIRECTORY, MODEL_ID, MODEL_BASENAME
+from db_mng import DB_Management
 
 # API queue addition
 from threading import Lock
@@ -31,7 +35,8 @@ elif torch.cuda.is_available():
     DEVICE_TYPE = "cuda"
 else:
     DEVICE_TYPE = "cpu"
-
+    
+print(DEVICE_TYPE)
 SHOW_SOURCES = True
 logging.info(f"Running on: {DEVICE_TYPE}")
 logging.info(f"Display Source Documents set to: {SHOW_SOURCES}")
@@ -60,31 +65,57 @@ EMBEDDINGS = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, mode
 #     )
 
 # load the vectorstore
-DB = Chroma(
-    persist_directory=PERSIST_DIRECTORY,
-    embedding_function=EMBEDDINGS,
-    client_settings=CHROMA_SETTINGS,
-)
-
-RETRIEVER = DB.as_retriever()
-
 LLM = load_model(device_type=DEVICE_TYPE, model_id=MODEL_ID, model_basename=MODEL_BASENAME)
-prompt, memory = get_prompt_template(promptTemplate_type="llama", history=False)
+DB = None
+RETRIEVER = None
+RETRIEVER_BM25 = None
+QA = None
 
-QA = RetrievalQA.from_chain_type(
-    llm=LLM,
-    chain_type="stuff",
-    retriever=RETRIEVER,
-    return_source_documents=SHOW_SOURCES,
-    chain_type_kwargs={
-        "prompt": prompt,
-    },
-)
+def load_DB():
+    global DB
+    global RETRIEVER
+    global RETRIEVER_BM25
+    global QA
+    global app
+    # load the vectorstore
+    
+    # Return document size
+    k = 4
+    
+    DB = Chroma(
+        persist_directory=PERSIST_DIRECTORY,
+        embedding_function=EMBEDDINGS,
+        client_settings=CHROMA_SETTINGS,
+    )
+    app.logger.info(f'DB size: {DB._collection.count()}')
+
+    RETRIEVER = DB.as_retriever(search_kwargs={"k": k * 2})
+    
+    collections = DB.get()
+    documents = [Document(page_content=c, metadata=m) for m, c in zip(collections['metadatas'], collections['documents'])]
+    RETRIEVER_BM25 = BM25Retriever.from_documents(documents=documents, preprocess_func=clean_text, k=k)
+    
+    prompt, memory = get_prompt_template(promptTemplate_type="llama", history=False)
+
+    QA = RetrievalQA.from_chain_type(
+        llm=LLM,
+        chain_type="stuff",
+        retriever=RETRIEVER,
+        retriever_bm25=RETRIEVER_BM25,
+        return_source_documents=SHOW_SOURCES,
+        chain_type_kwargs={
+            "prompt": prompt,
+        },
+    )
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO) 
+load_DB()
 
+# DB object init
+db_manager = DB_Management(f'{PERSIST_DIRECTORY}/mapping.json', PERSIST_DIRECTORY)
 
-@app.route("/api/delete_source", methods=["GET"])
+@app.route("/api/delete_source", methods=["DELETE"])
 def delete_source_route():
     folder_name = "SOURCE_DOCUMENTS"
 
@@ -105,29 +136,21 @@ def save_document_route():
         return "No selected file", 400
     if file:
         filename = secure_filename(file.filename)
-        folder_path = "SOURCE_DOCUMENTS"
+        folder_path = "SOURCE_TMP"
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
+            
         file_path = os.path.join(folder_path, filename)
+
         file.save(file_path)
         return "File saved successfully", 200
 
-
-@app.route("/api/run_ingest", methods=["GET"])
-def run_ingest_route():
-    global DB
-    global RETRIEVER
-    global QA
+@app.route("/api/run_add", methods=["POST"])
+def run_add():
+    
     try:
-        if os.path.exists(PERSIST_DIRECTORY):
-            try:
-                shutil.rmtree(PERSIST_DIRECTORY)
-            except OSError as e:
-                print(f"Error: {e.filename} - {e.strerror}.")
-        else:
-            print("The directory does not exist")
-
-        run_langest_commands = ["python", "ingest.py"]
+        run_langest_commands = ["python", "pipeline.py", "--source_dir", "SOURCE_TMP", "--parse_dir", "PARSED_TMP"]
+        
         if DEVICE_TYPE == "cpu":
             run_langest_commands.append("--device_type")
             run_langest_commands.append(DEVICE_TYPE)
@@ -135,29 +158,79 @@ def run_ingest_route():
         result = subprocess.run(run_langest_commands, capture_output=True)
         if result.returncode != 0:
             return "Script execution failed: {}".format(result.stderr.decode("utf-8")), 500
-        # load the vectorstore
-        DB = Chroma(
-            persist_directory=PERSIST_DIRECTORY,
-            embedding_function=EMBEDDINGS,
-            client_settings=CHROMA_SETTINGS,
-        )
-        RETRIEVER = DB.as_retriever()
-        prompt, memory = get_prompt_template(promptTemplate_type="llama", history=False)
-
-        QA = RetrievalQA.from_chain_type(
-            llm=LLM,
-            chain_type="stuff",
-            retriever=RETRIEVER,
-            return_source_documents=SHOW_SOURCES,
-            chain_type_kwargs={
-                "prompt": prompt,
-            },
-        )
+        
+        shutil.copytree("SOURCE_TMP", "SOURCE_DOCUMENTS", dirs_exist_ok=True)
+        shutil.copytree("PARSED_TMP", "PARSED_DOCUMENTS", dirs_exist_ok=True)
+        shutil.rmtree("SOURCE_TMP")
+        shutil.rmtree("PARSED_TMP")
+        
+        load_DB()
         return "Script executed successfully: {}".format(result.stdout.decode("utf-8")), 200
     except Exception as e:
         return f"Error occurred: {str(e)}", 500
 
+@app.route("/api/run_delete", methods=["DELETE"])
+def run_delete():
 
+    try:
+        # Cheng-Ping Love you very much.
+        global request_lock  # Make sure to use the global lock instance
+        _id = request.form.get("id")
+        # _id = os.path.join("PARSED_DOCUMENTS", _id)
+        app.logger.info(_id)
+        
+        with request_lock:
+            db_manager.delete_text(_id)
+
+        load_DB()
+        return "Script executed successfully", 200
+    except Exception as e:
+        return f"Error occurred: {str(e)}", 500
+
+
+@app.route("/api/run_update", methods=["PUT"])
+def run_update():
+    try:
+        global request_lock  # Make sure to use the global lock instance
+        # original_result is a jsonify dict
+        _id, revise_result = request.form.get("id"), request.form.get("revise_result")
+        app.logger.info(_id)
+        app.logger.info(revise_result)
+        
+        with request_lock:
+            db_manager.update_text(_id, revise_result.strip())
+
+        load_DB()
+        return "Script executed successfully", 200
+    except Exception as e:
+        return f"Error occurred: {str(e)}", 500
+    
+
+@app.route("/api/run_reset", methods=["POST", "DELETE"])
+def run_reset():
+    try:
+        db_manager.delete_db()
+        # result = subprocess.run(["python", "db_management.py", "--delete_db"], capture_output=True)
+        
+        run_langest_commands = ["python", "pipeline.py", "--source_dir", "SOURCE_TMP", "--parse_dir", "PARSED_TMP"]
+        if DEVICE_TYPE == "cpu":
+            run_langest_commands.append("--device_type")
+            run_langest_commands.append(DEVICE_TYPE)
+
+        result = subprocess.run(run_langest_commands, capture_output=True)
+        if result.returncode != 0:
+            return "Script execution failed: {}".format(result.stderr.decode("utf-8")), 500
+        
+        shutil.rmtree("SOURCE_DOCUMENTS")
+        shutil.rmtree("PARSED_DOCUMENTS")
+        shutil.move("SOURCE_TMP", "SOURCE_DOCUMENTS")
+        shutil.move("PARSED_TMP", "PARSED_DOCUMENTS")
+
+        load_DB()
+        return "Script executed successfully: {}".format(result.stdout.decode("utf-8")), 200
+    except Exception as e:
+        return f"Error occurred: {str(e)}", 500
+    
 @app.route("/api/prompt_route", methods=["GET", "POST"])
 def prompt_route():
     global QA
@@ -178,9 +251,9 @@ def prompt_route():
 
             prompt_response_dict["Sources"] = []
             for document in docs:
-                prompt_response_dict["Sources"].append(
-                    (os.path.basename(str(document.metadata["source"])), str(document.page_content))
-                )
+                _id = document.metadata["id"]
+                source = "/".join(str(document.metadata["source"]).split("/")[-3:])
+                prompt_response_dict["Sources"].append((_id, source, str(document.page_content)))
 
         return jsonify(prompt_response_dict), 200
     else:

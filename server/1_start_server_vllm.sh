5841ec1e76d5a2a6a6c49d13a76e3643b08ccbb8
ChungYujoyce <joyce.bhps@gmail.com>
2024-04-24 22:58:50 -0700

    add vllm
    Signed-off-by: ChungYujoyce <joyce.bhps@gmail.com>

--- server/1_start_server_vllm.sh
+++ server/1_start_server_vllm.sh
@@ -1,7 +1,11 @@
-CUDA_VISIBLE_DEVICES=0,1 python vllm_server.py \
-    --model=/workspace/models/models--mistralai--Mixtral-8x7B-Instruct-v0.1/snapshots/1e637f2d7cb0a9d6fb1922f305cb784995190a83 \
-    --max-model-len=128 \
-    --tensor-parallel-size=2 \
+HF_MODEL="meta-llama/Meta-Llama-3-8B-Instruct"
+HF_CHECKPOINT_PATH="/data/nlp/${HF_MODEL}"
+
+CUDA_VISIBLE_DEVICES=0 python -m vllm.entrypoints.openai.api_server  \
+    --model=${HF_CHECKPOINT_PATH} \
+    --served-model-name='llama3-8b-instruct' \
+    --max-model-len=4096 \
+    --tensor-parallel-size=1 \
     --pipeline-parallel-size=1 \
     --trust-remote-code \
     --dtype bfloat16 \
